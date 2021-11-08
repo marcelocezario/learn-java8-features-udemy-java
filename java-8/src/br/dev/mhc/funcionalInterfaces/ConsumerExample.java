f44e141c6c4aad294275229318ab957c353391c5
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-08 15:43:00 -0300

    Create ConsumerExample.java

---
+++ java-8/src/br/dev/mhc/funcionalInterfaces/ConsumerExample.java
@@ -0,0 +1,55 @@
+package br.dev.mhc.funcionalInterfaces;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import br.dev.mhc.data.Student;
+import br.dev.mhc.data.StudentDataBase;
+
+public class ConsumerExample {
+
+	static Consumer<Student> c2 = (student) -> System.out.println(student);
+	static Consumer<Student> c3 = (student) -> System.out.print(student.getName());
+	static Consumer<Student> c4 = (student) -> System.out.println(student.getActivities());
+
+	public static void printStudents() {
+
+		System.out.println("printStudents :");
+
+		List<Student> studentList = StudentDataBase.getAllStudents();
+		studentList.forEach(c2);
+	}
+
+	public static void printNameAndActivities() {
+
+		System.out.println("printNameAndActivities :");
+
+		List<Student> studentList = StudentDataBase.getAllStudents();
+
+		studentList.forEach(c3.andThen(c4)); // consumer chaining
+	}
+
+	public static void printNameAndActivitiesUsingCondition() {
+
+		System.out.println("printNameAndActivitiesUsingCondition :");
+
+		List<Student> studentList = StudentDataBase.getAllStudents();
+
+		studentList.forEach((student -> {
+			if (student.getGradeLevel() >= 3 && student.getGpa() >= 3.9) {
+				c3.andThen(c4).andThen(c2).accept(student);
+			}
+		})); // consumer chaining
+	}
+
+	public static void main(String[] args) {
+
+		Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
+
+		c1.accept("java8");
+		// printStudents();
+		printNameAndActivities();
+		printNameAndActivitiesUsingCondition();
+	}
+
+}
