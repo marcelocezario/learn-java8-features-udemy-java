37e8654a580d87872201b0b9c045a3071e06c391
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-09 17:14:50 -0300

    Create UnaryOperatorExample.java

---
+++ java-8/src/br/dev/mhc/funcionalInterfaces/UnaryOperatorExample.java
@@ -0,0 +1,15 @@
+package br.dev.mhc.funcionalInterfaces;
+
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorExample {
+	
+	static UnaryOperator<String> unaryOperator = (s) -> s.concat("Default");
+
+	public static void main(String[] args) {
+		
+		System.out.println(unaryOperator.apply("java8"));
+		
+	}
+
+}
