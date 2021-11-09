04b13d0643839e1eb4f9249e15805710cb628878
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-09 18:14:56 -0300

    Local variables in lambda expression

---
+++ java-8/src/br/dev/mhc/lambdas/LambdaVariable2.java
@@ -0,0 +1,24 @@
+package br.dev.mhc.lambdas;
+
+import java.util.function.Consumer;
+
+public class LambdaVariable2 {
+	
+	static int value = 4;
+	
+	public static void main(String[] args) {
+		
+		 //local variable
+		
+		Consumer<Integer> c1 = (i) -> {
+			value++;
+			System.out.println(value + i);
+		};
+		
+		//value = 6;
+		
+		c1.accept(4);
+		
+	}
+
+}
