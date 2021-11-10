e9ca581e883ccea6a1042c2ae6e63237d7fda737
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-10 14:05:07 -0300

    Update StreamsFlatMapExample.java

--- java-8/src/br/dev/mhc/streams/StreamsFlatMapExample.java
+++ java-8/src/br/dev/mhc/streams/StreamsFlatMapExample.java
@@ -12,14 +12,26 @@ public class StreamsFlatMapExample {
 		List<String> studentActivities = StudentDataBase.getAllStudents().stream() // Stream<Student>
 				.map(Student::getActivities) // Stream<List<String>>
 				.flatMap(List::stream) //Stream<String>
+				.distinct()
 				.collect(Collectors.toList());
 
 		return studentActivities;
+	}
+	
+	public static long getStudentActivitiesCount() {
+		long noOfStudentActivities = StudentDataBase.getAllStudents().stream() // Stream<Student>
+				.map(Student::getActivities) // Stream<List<String>>
+				.flatMap(List::stream) //Stream<String>
+				.distinct() //String<String> -> with distinct function performed
+				.count();
+
+		return noOfStudentActivities;
 	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("printStudentActivities : " + printStudentActivites()); 
+		System.out.println("getStudentActivitiesCount : " + getStudentActivitiesCount()); 
 	}
 
 }
