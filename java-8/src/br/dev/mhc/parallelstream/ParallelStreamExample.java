59d59b448e63e8d95a5ddffc88da2db2c72a24e1
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-11 10:24:08 -0300

    Create ParallelStreamExample.java

---
+++ java-8/src/br/dev/mhc/parallelstream/ParallelStreamExample.java
@@ -0,0 +1,27 @@
+package br.dev.mhc.parallelstream;
+
+import java.util.stream.IntStream;
+
+public class ParallelStreamExample {
+	
+	public static int sum_sequential_Stream() {
+		
+		return IntStream.rangeClosed(1, 100000)
+				.sum();
+	}
+	
+	public static int sum_parallel_Stream() {
+		
+		return IntStream.rangeClosed(1, 100000)
+				.sum();
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(sum_sequential_Stream());
+		
+		System.out.println(sum_parallel_Stream());
+
+	}
+
+}
