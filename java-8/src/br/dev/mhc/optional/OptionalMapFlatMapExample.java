1e94c30c7583e6d29fbe2e291672f374fddcc9f4
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-11 12:15:48 -0300

    Create OptionalMapFlatMapExample.java

---
+++ java-8/src/br/dev/mhc/optional/OptionalMapFlatMapExample.java
@@ -0,0 +1,43 @@
+package br.dev.mhc.optional;
+
+import java.util.Optional;
+
+import br.dev.mhc.data.Student;
+import br.dev.mhc.data.StudentDataBase;
+
+public class OptionalMapFlatMapExample {
+	
+	//filter
+	public static void optionalFilter() {
+		Optional<Student> studentOptional = Optional.ofNullable(StudentDataBase.studentSupplier.get()); // Optional<Student>
+		
+		studentOptional
+			.filter(student -> student.getGpa() >= 4.0)
+			.ifPresent(student -> System.out.println(student));
+	}
+	
+	//map
+	public static void optionalMap() {
+		Optional<Student> studentOptional = Optional.ofNullable(StudentDataBase.studentSupplier.get()); // Optional<Student>
+		
+		if(studentOptional.isPresent()) {
+			Optional<String> stringOptional = studentOptional
+					.filter(student -> student.getGpa() >= 3.5)
+					.map(Student::getName);
+			System.out.println(stringOptional.get());
+		}
+		
+		studentOptional
+			.filter(student -> student.getGpa() >= 4.0)
+			.ifPresent(student -> System.out.println(student));
+	}
+	
+	//flatmap
+
+	public static void main(String[] args) {
+
+		optionalFilter();
+		optionalMap();
+	}
+
+}
