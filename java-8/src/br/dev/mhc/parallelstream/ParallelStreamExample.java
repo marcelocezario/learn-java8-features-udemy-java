6ad2ef874878399912e325ebb310049ea01918ce
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-11 10:31:36 -0300

    Update ParallelStreamExample.java

--- java-8/src/br/dev/mhc/parallelstream/ParallelStreamExample.java
+++ java-8/src/br/dev/mhc/parallelstream/ParallelStreamExample.java
@@ -1,26 +1,38 @@
 package br.dev.mhc.parallelstream;
 
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class ParallelStreamExample {
 	
-	public static int sum_sequential_Stream() {
+	public static long checkPerfomanceResult(Supplier<Integer> supplier, int numberOfTimes) {
+		
+		long startTime = System.currentTimeMillis();
+		for(int i=0; i < numberOfTimes; i++) {
+			supplier.get();
+		}
+		long endTime = System.currentTimeMillis();
+		return endTime - startTime;
+	}
+	
+	public static int sumSequentialStream() {
 		
 		return IntStream.rangeClosed(1, 100000)
 				.sum();
 	}
 	
-	public static int sum_parallel_Stream() {
+	public static int sumParallelStream() {
 		
 		return IntStream.rangeClosed(1, 100000)
+				.parallel()
 				.sum();
 	}
 
 	public static void main(String[] args) {
 		
-		System.out.println(sum_sequential_Stream());
+		System.out.println("Sequential Stream Result : " + checkPerfomanceResult(ParallelStreamExample::sumSequentialStream, 20));
 		
-		System.out.println(sum_parallel_Stream());
+		System.out.println("Parallel Stream Result : " + checkPerfomanceResult(ParallelStreamExample::sumParallelStream, 20));
 
 	}
 
