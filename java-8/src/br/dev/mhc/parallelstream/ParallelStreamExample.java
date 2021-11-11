255029eff6d6348e8225003ce580dbe7eb4ce2e2
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-11 10:40:27 -0300

    Update ParallelStreamExample.java

--- java-8/src/br/dev/mhc/parallelstream/ParallelStreamExample.java
+++ java-8/src/br/dev/mhc/parallelstream/ParallelStreamExample.java
@@ -24,11 +24,13 @@ public class ParallelStreamExample {
 	public static int sumParallelStream() {
 		
 		return IntStream.rangeClosed(1, 100000)
-				.parallel()
+				.parallel() //split the data in to multiple parts
 				.sum();
 	}
 
 	public static void main(String[] args) {
+		
+		System.out.println(Runtime.getRuntime().availableProcessors());
 		
 		System.out.println("Sequential Stream Result : " + checkPerfomanceResult(ParallelStreamExample::sumSequentialStream, 20));
 		
